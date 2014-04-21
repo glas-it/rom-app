a2478d2db471f9af0dca08e315c40de161336ca0
Pablo.Ang <pablo.angelani@gmail.com>
2014-04-20 23:14:40 -0300

    fix llamadas al servidor

--- src/ar/com/glasit/rom/Service/WellKnownMethods.java
+++ src/ar/com/glasit/rom/Service/WellKnownMethods.java
@@ -2,7 +2,7 @@ package ar.com.glasit.rom.Service;
 
 public class WellKnownMethods {
 	/* Menu */
-    public final static String GetMenu= "menu.php";
-    public final static String GetTables= "mesas.php";
+    public final static String GetMenu= "restaurant/getMenu";
+    public final static String GetTables= "restaurant/mesas";
 	public final static String Login= "autenticacion/";
 }
