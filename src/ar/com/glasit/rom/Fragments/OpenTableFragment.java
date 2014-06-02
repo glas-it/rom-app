d81e5583a39fb83178228980b4312581285a4f52
Pablo.Ang <pablo.angelani@gmail.com>
2014-06-01 23:23:26 -0300

    fix cerrar mesa sin descuento

--- src/ar/com/glasit/rom/Fragments/OpenTableFragment.java
+++ src/ar/com/glasit/rom/Fragments/OpenTableFragment.java
@@ -462,6 +462,7 @@ public class OpenTableFragment extends SherlockFragment{
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 TablesGestor.getInstance().closeTable(getTable().getNumber());
+                                ((TableManager) getSherlockActivity()).onTableClosed(getTable().getId());
                             }
                         });
                         alert.show();
